ecdfaad8a0c55b5e510fe96a1dadc0422436a4c2
stevequindo <samquindo@hotmail.com>
2022-10-24 21:04:55 +1100

    ordering is corecto

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,7 +73,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allContentfulBlogPost {
+    allContentfulBlogPost(sort: {fields: createdAt, order: DESC}) {
       edges {
         node {
           title
